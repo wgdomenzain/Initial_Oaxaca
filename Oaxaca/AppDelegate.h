114e35db2db5a6c7f7f74ea5f5ffa64e5c3b2719
Walter Gonzalez Domenzain <walter.gonzalez@smartplace.mx>
2015-01-17 09:44:58 -0600

    Initial Commit

---
+++ Oaxaca/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Oaxaca
+//
+//  Created by Walter Gonzalez Domenzain on 17/01/15.
+//  Copyright (c) 2015 Smartplace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Oaxaca/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Oaxaca
+//
+//  Created by Walter Gonzalez Domenzain on 17/01/15.
+//  Copyright (c) 2015 Smartplace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
