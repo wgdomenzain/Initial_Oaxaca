ab03cefe71992479a5d8df1f9f581d25a589f9b8
Walter Gonzalez Domenzain <walter.gonzalez@smartplace.mx>
2015-01-17 12:34:16 -0600

    All Simpson family added. - They change forward and back when the button are pressed

--- Oaxaca/Welcome.m
+++ Oaxaca/Welcome.m
@@ -8,16 +8,22 @@
 
 #import "Welcome.h"
 
+NSMutableArray  *imagesArray;
+int             iImage = 0;
+
 @interface Welcome ()
 
 @end
+
 
 @implementation Welcome
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    //
+    imagesArray = [[NSMutableArray alloc] initWithObjects:@"homer01.png", @"marge01.gif", @"bart01.gif", @"lisa01.gif", @"maggie01.gif", nil];
+    
+    self.imgIntro.image = [UIImage imageNamed:imagesArray[iImage]];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -27,13 +33,29 @@
 
 - (IBAction)btnLoginPressed:(id)sender
 {
-    self.lblMsg.text = @"Lisa Simpson";
-    //self.view.backgroundColor = [UIColor orangeColor];
-    self.imgIntro.image = [UIImage imageNamed:@"lisa01.gif"];
+    if (iImage >= 4)
+    {
+        iImage = 0;
+    }
+    else
+    {
+        iImage++;
+    }
+    //self.lblMsg.text = @"Lisa Simpson";
+    self.imgIntro.image = [UIImage imageNamed:imagesArray[iImage]];
 }
 - (IBAction)btnRegisterPressed:(id)sender
 {
-    self.lblMsg.text = @"Bart Simpson";
-    self.imgIntro.image = [UIImage imageNamed:@"bart01.gif"];
+    if (iImage > 0)
+    {
+        iImage--;
+    }
+    else
+    {
+        iImage = 4;
+    }
+    
+    //self.lblMsg.text = @"Lisa Simpson";
+    self.imgIntro.image = [UIImage imageNamed:imagesArray[iImage]];
 }
 @end
